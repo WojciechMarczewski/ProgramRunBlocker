fee1cf8de548980aca79d5dc064527fd725f7145
Voyti <108059127+WojciechMarczewski@users.noreply.github.com>
2024-11-30 20:24:20 +0100

    Update ProgramRunManager.ps1
    Split issue fix - conversion to regex

--- Scripts/ProgramRunManager.ps1
+++ Scripts/ProgramRunManager.ps1
@@ -6,18 +6,29 @@ param (
     [string]$TaskName = "ProgramRunBlockerTask"
 )
 
+# Function to split arguments considering quotes
+function Split-Arguments {
+    param (
+        [string]$arguments
+    )
+
+    # Regular expression to split arguments considering quotes
+    $regex = '(?<=^| )(\"[^\"]+\"|\S+)(?=$| )'
+    return [regex]::matches($arguments, $regex) | ForEach-Object { $_.Value.Trim() }
+}
+
 # Check if an existing task exists and retrieve its values
 $existingTask = Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue
 
 if ($existingTask) {
-    $arguments = $existingTask.Actions[0].Arguments -split ' '
-    if ($arguments.Length -ge 5) { $SelectedFolder = $arguments[4] }
+    $arguments = Split-Arguments -arguments $existingTask.Actions[0].Arguments
+    if ($arguments.Length -ge 5) { $SelectedFolder = $arguments[4].Trim('"') }
     if ($arguments.Length -ge 6) { $StartTime = $arguments[5] }
     if ($arguments.Length -ge 7) { $EndTime = $arguments[6] }
 }
 
-# Add '*' to the folder path and ensure the path is not enclosed in quotes
-$SelectedFolder = "$SelectedFolder\*" -replace '"', '' 
+# Add '*' to the folder path
+$SelectedFolder = "$SelectedFolder\*"
 
 # Paths to Allowed and Disallowed scripts
 $allowedScript = ".\ProgramRunAllowed.ps1"
@@ -35,11 +46,11 @@ cd $scriptDirectory
 
 # Check if the current time is between StartTime and EndTime
 if (($currentHourMinuteSecond -ge $StartTime) -or ($currentHourMinuteSecond -lt $EndTime)) {
-    Write-Output "The current time is between $StartTime and $EndTime. Running Disallowed script."
-    & $disallowedScript $SelectedFolder
-} else {
-    Write-Output "The current time is not between $StartTime and $EndTime. Running Allowed script."
+    Write-Output "The current time is between $StartTime and $EndTime. Running Allowed script."
     & $allowedScript $SelectedFolder
+} else {
+    Write-Output "The current time is not between $StartTime and $EndTime. Running Disallowed script."
+    & $disallowedScript $SelectedFolder
 }
 gpupdate /force
 
